dcd503281f349ccb65faceba6297a2e935538d4e
K-osA <evbayooooo@gmail.com>
2022-02-02 18:58:15 +0900

    Implement usd<->coins

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [dollars, setDollars] = useState(0);
+  const [index, setIndex] = useState(-1);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,17 +14,41 @@ function App() {
         setLoading(false);
       });
   }, []);
+  const onChange = (event) => setDollars(event.target.value);
+  const onChangeSelect = (event) => setIndex(event.target.value);
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? null : (
+        <form>
+          <input
+            value={dollars}
+            onChange={onChange}
+            type="number"
+            placeholder="Dollars"
+          ></input>
+        </form>
+      )}
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onChangeSelect}>
+            <option value={-1}>Please select coin</option>
+            {coins.map((coin, index) => (
+              <option value={index}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          {index === -1 ? null : (
+            <h2>
+              You can buy {dollars / coins[index].quotes.USD.price}{" "}
+              {coins[index].name} for ${dollars} USD
+            </h2>
+          )}
+        </div>
+      )}
     </div>
   );
 }
